4199e31c512ae9eb509c84e085ed39de0e453ba3
Roberto Ramirez <robert.624@hotmail.com>
2023-07-10 15:02:46 -0500

    added each theme color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.scss";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface ThemeSwitcherProps {
+    setTheme: (theme: string) => void;
 }
 
-export default App
+export function ThemeSwitcher({ setTheme}: ThemeSwitcherProps) {
+
+
+    const handleTheme = (e: React.ChangeEvent) => {
+        const { id } = e.target;
+        setTheme(id);
+    };
+
+    return (
+        <div className="theme-switcher">
+            <label htmlFor="theme1">
+                <input
+                    name="theme"
+                    defaultChecked={true}
+                    id="theme1"
+                    type="radio"
+                    onChange={handleTheme}
+                />
+            </label>
+            <label htmlFor="theme2">
+                <input
+                    name="theme"
+                    id="theme2"
+                    type="radio"
+                    onChange={handleTheme}
+                />
+            </label>
+            <label htmlFor="theme3">
+                <input
+                    name="theme"
+                    id="theme3"
+                    type="radio"
+                    onChange={handleTheme}
+                />
+            </label>
+        </div>
+    );
+}
+
+function App() {
+    const [theme, setTheme] = useState<string>("theme1");
+
+    useEffect(() => {
+        document.body.setAttribute("data-theme", theme);
+    }, [theme]);
+
+
+    return (
+            <div className={`container`}>
+                <div className="top">
+                    <h1>calc</h1>
+                    <ThemeSwitcher setTheme={setTheme} />
+                </div>
+            </div>
+    );
+}
+
+export default App;
